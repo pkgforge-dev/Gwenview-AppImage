79c2585c4d9bb670571ce0956db5c9914989f280
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-03 05:41:21 -0400

    `kiconthemes-mini` is now provided by the debloated packages repo

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,15 +10,10 @@ pacman -Syu --noconfirm gwenview qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini kiconthemes-mini
 
 # remove qt6webengine, otherwise it gets deployed due to it having a qml plugin
 pacman -Rdd --noconfirm qt6-webengine
-
-# build kiconthemes without mandatory link to massive libKF6BreezeIcons.so library
-PRE_BUILD_CMDS="
-  sed -i -e 's|-DBUILD_TESTING=OFF|-DBUILD_TESTING=OFF -DUSE_BreezeIcons=OFF|g' ./PKGBUILD
-" make-aur-package --archlinux-pkg kiconthemes
 
 # If the application needs to be manually built that has to be done down here
 
