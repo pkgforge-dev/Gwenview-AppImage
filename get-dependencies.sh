f7c1414c5cd31b7b906980ce07968f339e8e8cf2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-11 17:24:47 -0400

    add more platformthemes

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm gwenview qt6ct
+pacman -Syu --noconfirm gwenview qt6ct kvantum lxqt-qtplugin breeze oxygen
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
